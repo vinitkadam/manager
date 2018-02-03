4f7c2370e7c220072185f700731ab71e83ada7c9
vinitkadam <vinitkadam1997@gmail.com>
2018-02-03 18:43:08 +0530

    minor changes

--- src/components/EmployeeCreate.js
+++ src/components/EmployeeCreate.js
@@ -1,7 +1,15 @@
 import React, { Component } from 'react';
+import { Picker, Text } from 'react-native';
+import { connect } from 'react-redux';
 import { Card, CardSection, Input, Button } from './common';
+import { employeeUpdate, employeeCreate } from '../actions';
 
 class EmployeeCreate extends Component {
+  onButtonPress() {
+    const { name, phone, shift } = this.props;
+
+    this.props.employeeCreate({ name, phone, shift: shift || 'Monday' });
+  }
   render() {
     return (
       <Card>
@@ -9,6 +17,8 @@ class EmployeeCreate extends Component {
           <Input
             label="Name"
             placeholder="Vinit"
+            value={this.props.name}
+            onChangeText={value => this.props.employeeUpdate({ prop: 'name', value })}
           />
         </CardSection>
 
@@ -16,11 +26,31 @@ class EmployeeCreate extends Component {
           <Input
             label="Phone"
             placeholder="777-777-7777"
+            value={this.props.phone}
+            onChangeText={value => this.props.employeeUpdate({ prop: 'phone', value })}
           />
         </CardSection>
 
+        <CardSection style={{ flexDirection: 'column' }}>
+          <Text style={{ fontSize: 20, paddingLeft: 20 }}>
+            Shift
+          </Text>
+          <Picker
+            selectedValue={this.props.shift}
+            onValueChange={value => this.props.employeeUpdate({ prop: 'shift', value })}
+          >
+            <Picker.Item label="Monday" value="Monday" />
+            <Picker.Item label="Tuesday" value="Tuesday" />
+            <Picker.Item label="Wednesday" value="Wednesday" />
+            <Picker.Item label="Thursday" value="Thursday" />
+            <Picker.Item label="Friday" value="Friday" />
+            <Picker.Item label="Saturday" value="Saturday" />
+            <Picker.Item label="Sunday" value="Sunday" />
+          </Picker>
+        </CardSection>
+
         <CardSection>
-          <Button>
+          <Button onPress={this.onButtonPress.bind(this)}>
             CREATE
           </Button>
         </CardSection>
@@ -29,4 +59,10 @@ class EmployeeCreate extends Component {
   }
 }
 
-export default EmployeeCreate;
+const mapStateToProps = (state) => {
+  const { name, phone, shift } = state.employeeForm;
+
+  return { name, phone, shift };
+};
+
+export default connect(mapStateToProps, { employeeUpdate, employeeCreate })(EmployeeCreate);
--- src/components/EmployeeList.js
+++ src/components/EmployeeList.js
@@ -1,20 +1,51 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import _ from 'lodash';
+import { ListView, View, Text } from 'react-native';
+import { connect } from 'react-redux';
+import { employeesFetch } from '../actions';
 
 class EmployeeList extends Component {
+  componentWillMount() {
+    console.log('componentWillMount invoked');
+    this.props.employeesFetch();
+
+    //render the list of employees
+    //this.createDataSource(this.props);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    // nextProps are the next set of props that this component
+    // will be rendered cloneWithRows
+    // this.props will still be the old set of props
+
+    //render list of employees after fetching from firebase
+    console.log('componentWillReceiveProps invoked');
+    this.createDataSource(nextProps);
+  }
+
+  createDataSource({ employees }) {
+    const ds = new ListView.DataSource({
+      rowHasChanged: (r1, r2) => r1 !== r2
+    });
+
+    this.DataSource = ds.cloneWithRows(employees);
+  }
+
   render() {
+    console.log(this.props);
     return (
-      <View>
-        <Text>Employee List</Text>
-        <Text>Employee List</Text>
-        <Text>Employee List</Text>
-        <Text>Employee List</Text>
-        <Text>Employee List</Text>
-        <Text>Employee List</Text>
-        <Text>Employee List</Text>
-      </View>
+      <View />
     );
   }
 }
 
-export default EmployeeList;
+const mapStateToProps = state => {
+  const employees = _.map(state.employees, (val, uid) => {
+    return { ...val, uid };
+  });
+  console.log({ employees });
+
+  return { employees };
+};
+
+export default connect(null, { employeesFetch })(EmployeeList);
